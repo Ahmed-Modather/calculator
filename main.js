756b6bbb7a07d63444ee89c0df814774e9b2cc8b
Ahmed Modather <115614683+Ahmed-Modather@users.noreply.github.com>
2022-11-19 08:54:32 +0200

    Create constructor function to create number
    it create object can repressent number with useful methods to handle it.

--- main.js
+++ main.js
@@ -32,8 +32,28 @@ function operate(operator, number1, number2) {
 	}
 }
 
-// Function Show Its argument in the top side of calculator's screen.
-function showInScreen (value) {
+// Function Shows Its argument in the top side of calculator's screen.
+function viewInScreen(value) {
 	const topScreen = document.querySelector('.top-screen');
 	topScreen.textContent = value;
+}
+
+/* Constructor Function used to create number objects by add its digits one by one 
+   and finally we can get the whole number.
+   it allows to remove digit too.
+   Note: In case there is no input it return zero*/
+function Number() {
+	this.digits = [];
+
+	this.addDigit = function (digit) {
+		this.digits.push(digit);
+	};
+
+	this.returnNumber = function () {
+		return +this.digits.join('');
+	};
+
+	this.removeDigit = function () {
+		this.digits.pop();
+	};
 }
